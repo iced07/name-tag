b75720c6cc2c69d66df09774233c58ffa22b4e75
iced07 <67426378+iced07@users.noreply.github.com>
2020-07-05 10:44:03 +0800

    MakeCode 项目的初始文件

---
+++ main.py
@@ -0,0 +1,10 @@
+def on_received_string(receivedString):
+    pass
+radio.on_received_string(on_received_string)
+
+basic.show_string("Hello!")
+basic.show_string("cbb")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,8 @@
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    
+})
+basic.showString("Hello!")
+basic.showString("cbb")
+basic.forever(function on_forever() {
+    
+})
